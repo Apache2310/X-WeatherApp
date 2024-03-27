1e7ee77ecc04e96b289518f8cbba7d0d993d25f1
Siddhant Patwa <siddhantpatwa8@gmail.com>
2024-03-27 14:14:14 +0530

    key

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const WeatherDisplay = ({ city }) => {
       axios
         .get(`https://api.weatherapi.com/v1/current.json`, {
           params: {
-            key: "cf6cae627141447e9e6113102230410",
+            key: "e389fc1a3e6c406db1093255231012",
             q: city
           }
         })
